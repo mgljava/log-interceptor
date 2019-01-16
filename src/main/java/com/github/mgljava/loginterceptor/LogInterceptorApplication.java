ec402f305a0726ba4905ca33a29773c17913978f
SweepMonk <guangliang1120@gmail.com>
2019-01-16 18:09:18 +0800

    [Dev][Monk] init project

---
+++ src/main/java/com/github/mgljava/loginterceptor/LogInterceptorApplication.java
@@ -0,0 +1,14 @@
+package com.github.mgljava.loginterceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogInterceptorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LogInterceptorApplication.class, args);
+  }
+
+}
+
